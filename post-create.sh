20f68ef94ed1ffa16273d1ab88b6379c58fa8d1a
Camilo Molano <camohe90@gmail.com>
2024-05-03 09:23:10 -0500

    Update post-create.sh

--- post-create.sh
+++ post-create.sh
@@ -4,8 +4,7 @@ echo "running"
 pipx install poetry
 pip install typing-extensions
 poetry init -n
-poetry add git+https://github.com/algorandfoundation/algokit-utils-py#feat/algorand_client
-poetry env use python3.12
+poetry add git+https://github.com/algorandfoundation/algokit-utils-py
 poetry shell
 
 
