88572975433cb9f84fe0a5264388a399cbc51f7f
Sara Jane (SJ) <127313669+Ganainmtech@users.noreply.github.com>
2024-08-13 12:03:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,16 @@
 
 echo "Running setup script..."
 
-# Install Poetry using pipx
 pipx install poetry
-
-# Initialize a Poetry project without any interactive prompts
+pip install typing-extensions
 poetry init -n
-
-# Add dependencies to the project
-poetry add typing-extensions
-poetry add fast-html htmx uvicorn
-
-# Clone and install the Algokit Utils package
 git clone https://github.com/algorandfoundation/algokit-utils-py.git
 cd algokit-utils-py
 pip install .
 cd ..
 
 echo "Setup completed."
+
+
+
+
