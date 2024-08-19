5fd552e3478cbe07aa75a501b38dbd152f12cc6f
Camilo Molano <camohe90@gmail.com>
2024-08-19 08:54:58 -0500

    Update algorand_setup.sh

--- algorand_setup.sh
+++ algorand_setup.sh
@@ -3,8 +3,8 @@
 echo "Configurando el entorno"
 pipx install poetry
 poetry init -n
-poetry env use python3.12
 pip install typing-extensions
+poetry env use python3.12
 poetry add git+https://github.com/algorandfoundation/algokit-utils-py
 poetry shell
 
