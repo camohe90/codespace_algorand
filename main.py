3c1f8d7ed9d90bcabfbd49bdf419394c2b6d375a
Sara Jane (SJ) <127313669+Ganainmtech@users.noreply.github.com>
2024-12-10 15:23:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from algokit_utils.beta.algorand_client import (AlgorandClient, ) # Import what you need here
 
+# Client to connect to localnet
+algorand = AlgorandClient.default_local_net()
+
+# Import dispenser from KMD 
+dispenser = algorand.account.dispenser()
+print("Dispenser Address: ", dispenser.address)
