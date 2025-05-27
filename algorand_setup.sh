731249643314d9211b11a6fb4649b01883c61848
Camilo Molano <camohe90@gmail.com>
2025-05-27 16:13:59 -0500

    Update algorand_setup.sh

--- algorand_setup.sh
+++ algorand_setup.sh
@@ -3,10 +3,9 @@
 echo "Configurando el entorno"
 pip install typing-extensions
 pip install python-dotenv
-pipx install poetry
-poetry init -n
-poetry add git+https://github.com/algorandfoundation/algokit-utils-py
-poetry shell
+python -m venv venv/
+source venv/bin/activate  
+pip install "algokit-utils"
 
 
 
